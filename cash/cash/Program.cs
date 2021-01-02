2cdc25baf79bba0240dc21a2f57e905698837255
Recep Deniz <61727501+bendenizrecep@users.noreply.github.com>
2021-01-02 15:15:50 +0300

    first commit
    when you input the float. use "," (e.g. 0,04). if you use "." c# willn't comprehend as float.

---
+++ cash/cash/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace cash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float xx;
+            do
+            {
+                Console.WriteLine("Change:");
+                xx = Convert.ToSingle(Console.ReadLine());
+            }
+            while (xx < 0);
+            int cevir = (int)Math.Round(xx * 100);
+            int para = 0;
+
+            while (cevir >= 25)
+            {
+                cevir -= 25;
+                para++;
+            }
+            while (cevir >= 10)
+            {
+                cevir -= 10;
+                para++;
+            }
+            while (cevir >= 5)
+            {
+                cevir -= 5;
+                para++;
+            }
+            while (cevir >= 1)
+            {
+                cevir -= 1;
+                para++;
+            }
+        Console.Write(para);
+        }
+    }
+}
