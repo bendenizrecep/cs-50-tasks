34887990a12e8347a25b4c3a2fdc9542e70b86ac
Recep Deniz <61727501+bendenizrecep@users.noreply.github.com>
2021-01-02 14:18:26 +0300

    Add files via upload

---
+++ mario advanced/mario advanced/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mario_advanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height;
+            do
+            {
+                Console.WriteLine("Height:");
+                height = Convert.ToInt32(Console.ReadLine());
+            }
+            while (height < 1 || height > 8);
+            for (int i = 1; i <= height; i++)
+            {
+                for (int j = height; j > i; j--)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 1; k <= i; k++)
+                {
+                    Console.Write("#");
+                }
+                Console.Write("  ");
+                for (int x = 1; x <= i; x++)
+                {
+                    Console.Write("#");
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
